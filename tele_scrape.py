f802c3d44de3e6c34010f9e7558f448f1503014c
ahcrej <jeremychai1998@gmail.com>
2023-12-21 00:38:05 +1100

    Basis to scrape a telegram channel.

---
+++ tele_scrape.py
@@ -0,0 +1,33 @@
+# source env/bin/activate
+from pyrogram import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CONFIG = {
+    "telegram_api_id": int(os.getenv("TG_API_ID")),
+    "telegram_hash": os.getenv("TG_API_HASH"),
+}
+
+app = Client("my_account",CONFIG["telegram_api_id"],CONFIG["telegram_hash"])
+
+chat_id = "nytimes"
+
+lst = []
+
+async def main():
+    async with app:
+        async for message in app.get_chat_history(chat_id):
+            msg = str(message.text)
+            if "Biden" in msg:
+                if msg not in lst:
+                    lst.append(msg)
+
+app.run(main())
+
+with app:
+    for i in lst:
+        app.send_message("me", i)
+
+print("done")
